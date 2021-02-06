72985694380e68fe68f0edf6cd5aa2d931a23198
Andrew Smirnov <smirnov@informatics.ru>
2021-02-06 19:46:01 +0300

    Add initial environment template

--- exchange_engine/urls.py
+++ exchange_engine/urls.py
@@ -1,4 +1,4 @@
-"""multistreamer URL Configuration
+"""exchange_engine URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- exchange_engine/wsgi.py
+++ exchange_engine/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for multistreamer project.
+WSGI config for exchange_engine project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multistreamer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exchange_engine.settings')
 
 application = get_wsgi_application()
