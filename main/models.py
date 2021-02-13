073cc776c36c2c3f300bd9ae4a878987d9cd4e1f
Кошелев Иван <vaniagav03@mail.ru>
2021-02-13 17:50:10 +0300

    adding a broadcast model

--- main/models.py
+++ main/models.py
@@ -1,1 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Broadcast(models.Model):
+    name = models.CharField(max_length=128)
+    url = models.URLField(max_length=128)
+    key = models.CharField(max_length=128)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
