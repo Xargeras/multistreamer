fae2108eb779c1412551ca30dad7994f6f0e5003
Arsenii Ulianov <sockwarior@gmail.com>
2021-02-16 00:07:08 +0300

    1

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_page(request):
+    context = {
+        'pagename': 'Главная',
+    }
+    return render(request, 'pages/index.html', context)
