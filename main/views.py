4e08e4da3b5239a0f465c0bcfe79584032910a2e
Баринов Арсений <BArseniyy@yandex.ru>
2021-02-17 20:18:46 +0300

    Creating an initial page template

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexPage(View):
+    context = {
+        'pagename': 'Главная',
+    }
+
+    def get(self, request):
+        return render(request, 'pages/index.html', self.context)
+
+
+class SettingProfileView(View):
+    context = {
+        'pagename': 'Настройки',
+    }
+
+    def get(self, request):
+        return render(request, 'pages/account.html', self.context)
+
+
