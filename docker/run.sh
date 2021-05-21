97cc2e369802c1dcc162b71523a38698af851398
Andrew Smirnov <smirnov@informatics.ru>
2021-05-21 19:35:17 +0300

    Fix supervisor in docker config

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd /ps102_multistreamer/ || exit 1
-python manage.py migrate
-python manage.py collectstatic --noinput
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 daphne -b 0.0.0.0 -p 8082 --access-log /var/log/ps102_multistreamer/daphne_access.log multistreamer.asgi:application
